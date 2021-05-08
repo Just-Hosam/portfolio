c369c8c23ae9022927086fbce3284fe28db087c6
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-07 23:28:09 -0600

    Details setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import Header from './Header';
+import Details from './Details';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<Details />
 		</div>
 	);
 }
---
+++ src/components/Details.jsx
@@ -0,0 +1,13 @@
+import Skills from './Skills';
+import Projects from './Projects';
+import Contact from './Contact';
+
+export default function Details() {
+	return (
+		<div id="details">
+			<Skills />
+			<Projects />
+			<Contact />
+		</div>
+	);
+}
