abbb9a496b5c92ed4be609399a94711cdcd184c4
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-10 13:05:19 -0600

    Footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import Header from './Header';
 import Details from './Details';
+import Footer from './Footer';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Details />
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Footer() {
+	return (
+		<div id="footer-main">
+			<p>Created by Moi</p>
+			<div>
+				<i className="fas fa-file-alt"></i>
+				<i className="fab fa-github"></i>
+				<i className="fab fa-linkedin-in"></i>
+			</div>
+		</div>
+	);
+}
