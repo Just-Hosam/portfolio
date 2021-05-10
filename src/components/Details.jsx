124967d736bbb44df869a6b76e58057e32c72884
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-10 14:26:46 -0600

    React Router setup

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,13 +1,34 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import Skills from './Skills';
-import Projects from './Projects';
+import Projects from './Projects/Projects';
 import Contact from './Contact';
+import AtHomeProject from './Projects/AtHomeProject';
+import NotSoSmartProject from './Projects/NotSoSmartProject';
+import SchedulerProject from './Projects/SchedulerProject';
 
 export default function Details() {
 	return (
-		<div id="details">
-			<Skills />
-			<Projects />
-			<Contact />
-		</div>
+		<Router>
+			<div id="details">
+				<Link to="/">MAIN</Link>
+				<Switch>
+					<Route path="/AtHome">
+						<AtHomeProject />
+					</Route>
+					<Route path="/NotSoSmart">
+						<NotSoSmartProject />
+					</Route>
+					<Route path="/Scheduler">
+						<SchedulerProject />
+					</Route>
+					<Route path="/">
+						<Skills />
+						<Projects />
+						<Contact />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 }
