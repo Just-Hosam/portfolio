73c1f3272fd730c7264d32a5a3e4a4dc7e1b25d8
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-13 01:32:50 -0600

    add links to icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,21 @@ import React from 'react';
 export default function Footer() {
 	return (
 		<div id="footer-main">
-			<p>Created by Moi</p>
+			<p>Created by meeeee &#169; 2021</p>
 			<div>
-				<i className="fas fa-file-alt"></i>
-				<i className="fab fa-github"></i>
-				<i className="fab fa-linkedin-in"></i>
+				<a
+					rel="noreferrer"
+					target="_blank"
+					href="https://www.canva.com/design/DAEa1quHBdE/Av2MQ9oi0e0aXV7EcNyfdw/view?utm_content=DAEa1quHBdE&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton"
+				>
+					<i className="fas fa-file-alt"></i>
+				</a>
+				<a rel="noreferrer" target="_blank" href="https://github.com/Just-Hosam">
+					<i className="fab fa-github"></i>
+				</a>
+				<a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/hosam-dahrooge/">
+					<i className="fab fa-linkedin-in"></i>
+				</a>
 			</div>
 		</div>
 	);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-export default function Header() {
+export default function Header(props) {
 	return (
 		<header id="header-main">
 			<div id="header-icons">
-				<i className="fas fa-file-alt"></i>
-				<i className="fab fa-github"></i>
-				<i className="fab fa-linkedin-in"></i>
+				<a
+					rel="noreferrer"
+					target="_blank"
+					href="https://www.canva.com/design/DAEa1quHBdE/Av2MQ9oi0e0aXV7EcNyfdw/view?utm_content=DAEa1quHBdE&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton"
+				>
+					<i className="fas fa-file-alt"></i>
+				</a>
+				<a rel="noreferrer" target="_blank" href="https://github.com/Just-Hosam">
+					<i className="fab fa-github"></i>
+				</a>
+				<a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/hosam-dahrooge/">
+					<i className="fab fa-linkedin-in"></i>
+				</a>
 			</div>
 			<h1>Hosam Dahrooge</h1>
 			<p>
@@ -15,7 +25,7 @@ export default function Header() {
 				am objective, cheerful, and love to learn. A small break for me a chance to learn about deep subjects
 				(CPU architectures and how CPUs read machine code).
 			</p>
-			<div id="header-more">
+			<div id="header-more" ref={props.myRef} onClick={props.executeScroll}>
 				<span>learn more</span>
 				<i className="fas fa-arrow-down"></i>
 			</div>
