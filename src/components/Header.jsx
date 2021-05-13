98bf45ea3c9744c19d621cf23986eedd937fe85b
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-12 22:28:44 -0600

    Add about me

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,12 +10,10 @@ export default function Header() {
 			</div>
 			<h1>Hosam Dahrooge</h1>
 			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-				industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-				into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-				release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-				software like Aldus PageMaker including versions of Lorem Ipsum.
+				I'm a full stack web developer. I am pivoting from a background in Petroleum Engineering to the
+				Software Development world as it is ever expanding with increasing complexity and problems to solve. I
+				am objective, cheerful, and love to learn. A small break for me a chance to learn about deep subjects
+				(CPU architectures and how CPUs read machine code).
 			</p>
 			<div id="header-more">
 				<span>learn more</span>
