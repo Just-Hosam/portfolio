51794a6386656525d37f5d8ce414782d79e2dc1f
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-13 01:35:12 -0600

    add scroll to project card

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,10 +1,16 @@
 export default function ProjectCard(props) {
+	const handleClick = (mode) => {
+		props.setMode(mode);
+		props.executeScroll();
+	};
+
 	return (
 		<div className="project-card" style={{ backgroundImage: props.data.bgColor }}>
 			<img src={props.data.logo} alt={props.data.title} />
 			<p>{props.data.description}</p>
-			<span onClick={() => props.setMode(props.data.mode)}>
-				learn more<i className="fas fa-long-arrow-alt-right"></i>
+			<span onClick={() => handleClick(props.data.mode)}>
+				learn more
+				<i className="fas fa-long-arrow-alt-right"></i>
 			</span>
 		</div>
 	);
