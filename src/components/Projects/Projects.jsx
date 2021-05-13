e9f946fb19ed2348eca121e5fb5004a419b1ac38
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-13 01:34:59 -0600

    separate project cards data

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,35 +1,10 @@
+import projectCards from '../../data/projectCards';
+
 import ProjectCard from './ProjectCard';
 
-const projectsData = [
-	{
-		title: '@Home',
-		logo: 'https://see.fontimg.com/api/renderfont4/d9nn7/eyJyIjoiZnMiLCJoIjo3MSwidyI6MTAwMCwiZnMiOjcxLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiIzI1MkMzNSIsInQiOjF9/QEhvbWU/candycane.png',
-		bgColor: 'linear-gradient(rgb(128, 118, 251), rgb(128, 118, 251))',
-		description: 'A productivity dashboard Built for families to help them organize their lives.',
-		link: '/portfolio/AtHome',
-		mode: '@HOME',
-	},
-	{
-		title: 'NotSoSmart',
-		logo: 'https://see.fontimg.com/api/renderfont4/w1l49/eyJyIjoiZnMiLCJoIjo2NCwidyI6MjAwMCwiZnMiOjMyLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/Tm90U29TbWFydA/marshmallow-personal-use-regular.png',
-		bgColor: 'linear-gradient(135deg, #e2c35d, #da785d',
-		description: 'A TODO list app that automatically categorizes the given input using a multitude of APIs.',
-		link: '/portfolio/NotSoSmart',
-		mode: 'NOTSOSMART',
-	},
-	{
-		title: 'Scheduler',
-		logo: 'https://see.fontimg.com/api/renderfont4/K7ORW/eyJyIjoiZnMiLCJoIjo1MCwidyI6MTAwMCwiZnMiOjUwLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiIzI1MkMzNSIsInQiOjF9/U2NoZWR1bGVy/rondal-regular.png',
-		bgColor: 'linear-gradient(#222f3e, #222f3e)',
-		description: 'Schedule appointments between interviewer and students.',
-		link: '/portfolio/Scheduler',
-		mode: 'SCHEDULER',
-	},
-];
-
 export default function Projects(props) {
-	const projectsComponents = projectsData.map((elem, i) => {
-		return <ProjectCard key={i} data={elem} setMode={props.setMode} />;
+	const projectsComponents = projectCards.map((elem, i) => {
+		return <ProjectCard key={i} data={elem} setMode={props.setMode} executeScroll={props.executeScroll} />;
 	});
 
 	return (
---
+++ src/data/projectCards.js
@@ -0,0 +1,37 @@
+// Data structure
+// const projectCard = [
+//   {
+//     title: 'title',
+//     logo: 'image link',
+//     bgColor: 'linear-gradient(colorOne, colorTwo)',
+//     description: 'mini desc',
+//     mode: 'mode keyword for conditional rerendering'
+//   }
+//   ...
+// ]
+
+const projectCards = [
+	{
+		title: '@Home',
+		logo: 'https://see.fontimg.com/api/renderfont4/d9nn7/eyJyIjoiZnMiLCJoIjo3MSwidyI6MTAwMCwiZnMiOjcxLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiIzI1MkMzNSIsInQiOjF9/QEhvbWU/candycane.png',
+		bgColor: 'linear-gradient(rgb(128, 118, 251), rgb(128, 118, 251))',
+		description: 'A productivity dashboard Built for families to help them organize their lives.',
+		mode: '@HOME',
+	},
+	{
+		title: 'NotSoSmart',
+		logo: 'https://see.fontimg.com/api/renderfont4/w1l49/eyJyIjoiZnMiLCJoIjo2NCwidyI6MjAwMCwiZnMiOjMyLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/Tm90U29TbWFydA/marshmallow-personal-use-regular.png',
+		bgColor: 'linear-gradient(135deg, #e2c35d, #da785d',
+		description: 'A TODO list app that automatically categorizes the given input using a multitude of APIs.',
+		mode: 'NOTSOSMART',
+	},
+	{
+		title: 'Scheduler',
+		logo: 'https://see.fontimg.com/api/renderfont4/K7ORW/eyJyIjoiZnMiLCJoIjo1MCwidyI6MTAwMCwiZnMiOjUwLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiIzI1MkMzNSIsInQiOjF9/U2NoZWR1bGVy/rondal-regular.png',
+		bgColor: 'linear-gradient(#222f3e, #222f3e)',
+		description: 'Schedule appointments between interviewer and students.',
+		mode: 'SCHEDULER',
+	},
+];
+
+export default projectCards;
