93484f64508f41ba5e7467d89b380d11ea7c0999
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-13 21:06:48 -0600

    update project card logos with higher quality imgs

--- src/data/projectCards.js
+++ src/data/projectCards.js
@@ -13,21 +13,21 @@
 const projectCards = [
 	{
 		title: '@Home',
-		logo: 'https://see.fontimg.com/api/renderfont4/d9nn7/eyJyIjoiZnMiLCJoIjo3MSwidyI6MTAwMCwiZnMiOjcxLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiIzI1MkMzNSIsInQiOjF9/QEhvbWU/candycane.png',
+		logo: 'https://see.fontimg.com/api/renderfont4/BVWx/eyJyIjoiZnMiLCJoIjoyMDAsInciOjEwMDAsImZzIjoyMDAsImZnYyI6IiNGRkZGRkYiLCJiZ2MiOiIjMzUzRDRCIiwidCI6MX0/QEhvbWU/bad-unicorn-demo-regular.png',
 		bgColor: 'linear-gradient(rgb(128, 118, 251), rgb(128, 118, 251))',
 		description: 'A productivity dashboard Built for families to help them organize their lives.',
 		mode: '@HOME',
 	},
 	{
 		title: 'NotSoSmart',
-		logo: 'https://see.fontimg.com/api/renderfont4/w1l49/eyJyIjoiZnMiLCJoIjo2NCwidyI6MjAwMCwiZnMiOjMyLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/Tm90U29TbWFydA/marshmallow-personal-use-regular.png',
+		logo: 'https://see.fontimg.com/api/renderfont4/w1l49/eyJyIjoiZnMiLCJoIjoyMDAsInciOjEwMDAsImZzIjoyMDAsImZnYyI6IiNGRkZGRkYiLCJiZ2MiOiIjMzUzRDRCIiwidCI6MX0/Tm90U29TbWFydA/marshmallow-personal-use-regular.png',
 		bgColor: 'linear-gradient(135deg, #e2c35d, #da785d',
 		description: 'A TODO list app that automatically categorizes the given input using a multitude of APIs.',
 		mode: 'NOTSOSMART',
 	},
 	{
 		title: 'Scheduler',
-		logo: 'https://see.fontimg.com/api/renderfont4/K7ORW/eyJyIjoiZnMiLCJoIjo1MCwidyI6MTAwMCwiZnMiOjUwLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiIzI1MkMzNSIsInQiOjF9/U2NoZWR1bGVy/rondal-regular.png',
+		logo: 'https://see.fontimg.com/api/renderfont4/9YzoK/eyJyIjoiZnMiLCJoIjoyMDAsInciOjEwMDAsImZzIjoyMDAsImZnYyI6IiNGRkZGRkYiLCJiZ2MiOiIjMzUzRDRCIiwidCI6MX0/U2NoZWR1bGVy/nugo-sans-light.png',
 		bgColor: 'linear-gradient(#222f3e, #222f3e)',
 		description: 'Schedule appointments between interviewer and students.',
 		mode: 'SCHEDULER',
