923625938d386e1640971a916ed975445473a8cf
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-06-07 23:18:03 -0600

    Add Wannabitly project card

--- src/data/projectCards.js
+++ src/data/projectCards.js
@@ -12,25 +12,27 @@
 
 const projectCards = [
 	{
+		title: 'WannaBitly',
+		logo: 'https://see.fontimg.com/api/renderfont4/W92V/eyJyIjoiZnMiLCJoIjoxNjgsInciOjEwMDAsImZzIjoxNjgsImZnYyI6IiNGRkZGRkYiLCJiZ2MiOiIjMzUzRDRCIiwidCI6MX0/V2FubmFCaXRseQ/rubik.png',
+		bgColor: 'linear-gradient(#202b3a, #202b3a)',
+		description: 'A Wannabe Bitly. Shortens your URLs and provides click analytics for each URL.',
+		mode: 'WANNABITLY',
+	},
+	{
 		title: '@Home',
 		logo: 'https://see.fontimg.com/api/renderfont4/BVWx/eyJyIjoiZnMiLCJoIjoyMDAsInciOjEwMDAsImZzIjoyMDAsImZnYyI6IiNGRkZGRkYiLCJiZ2MiOiIjMzUzRDRCIiwidCI6MX0/QEhvbWU/bad-unicorn-demo-regular.png',
 		bgColor: 'linear-gradient(rgb(128, 118, 251), rgb(128, 118, 251))',
 		description: 'A productivity dashboard built for families to help them organize their lives.',
 		mode: '@HOME',
 	},
+
 	{
 		title: 'NotSoSmart',
 		logo: 'https://see.fontimg.com/api/renderfont4/w1l49/eyJyIjoiZnMiLCJoIjoyMDAsInciOjEwMDAsImZzIjoyMDAsImZnYyI6IiNGRkZGRkYiLCJiZ2MiOiIjMzUzRDRCIiwidCI6MX0/Tm90U29TbWFydA/marshmallow-personal-use-regular.png',
 		bgColor: 'linear-gradient(135deg, #e2c35d, #da785d',
-		description: 'A TODO list app that automatically categorizes the given input using a multitude of APIs.',
+		description:
+			'A smart TODO list that automatically categorizes the given input using a multitude of APIs.',
 		mode: 'NOTSOSMART',
-	},
-	{
-		title: 'Scheduler',
-		logo: 'https://see.fontimg.com/api/renderfont4/9YzoK/eyJyIjoiZnMiLCJoIjoyMDAsInciOjEwMDAsImZzIjoyMDAsImZnYyI6IiNGRkZGRkYiLCJiZ2MiOiIjMzUzRDRCIiwidCI6MX0/U2NoZWR1bGVy/nugo-sans-light.png',
-		bgColor: 'linear-gradient(#222f3e, #222f3e)',
-		description: 'Schedule appointments between interviewer and students.',
-		mode: 'SCHEDULER',
 	},
 ];
 
